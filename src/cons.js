d80fbbb3fbaf2216548d8e97d09b4c89244aa6c5
Sunil Mali <sunilmalishukhala7023@gmail.com>
2021-12-01 17:40:36 +0530

    seccond com

---
+++ src/cons.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+
+    apiKey: "AIzaSyBjzKntLo_o-4t5oL6SXixBGlV7pK0bDkk",
+    authDomain: "psc-adda-4c147.firebaseapp.com",
+    databaseURL: "https://psc-adda-4c147-default-rtdb.firebaseio.com",
+    projectId: "psc-adda-4c147",
+    storageBucket: "psc-adda-4c147.appspot.com",
+    messagingSenderId: "638383089976",
+    appId: "1:638383089976:web:ce32eca4f48959b48779cd",  
+    measurementId: "G-J17NR8K9YS"  
+  };
+ 
+  export default firebaseConfig
--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,7 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth'
 import 'firebase/compat/firestore'
 import 'firebase/compat/storage'
-import config from './config'
+import config from './cons'
 firebase.initializeApp(config)
 
 const auth = firebase.auth()
